7b44817da46d66e8845c0527d85a3ed40b7046b6
armyhaylenko <armyhaylenko@gmail.com>
2021-06-20 15:05:46 +0300

    init

---
+++ lib.rs
@@ -0,0 +1,50 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use ink_lang as ink;
+
+use ink_prelude::*;
+
+
+#[ink::contract]
+mod swap {
+    use ink_storage::Lazy;
+    use erc20::stub::Erc20;
+    use brush::traits::InkStorage;
+
+    #[ink(storage)]
+    #[derive(Default)]
+    pub struct Swap {
+        token_addr: AccountId,
+        exchange_rate: Lazy<u8>,
+    }
+
+    impl InkStorage for Swap {}
+
+    impl Swap {
+        /// Constructor that initializes the `bool` value to the given `init_value`.
+        #[ink(constructor)]
+        pub fn new(token_addr: AccountId, exchange_rate: u8) -> Self {
+            Self { token_addr, exchange_rate: Lazy::new(exchange_rate) }
+        }
+
+        #[ink(message, payable)]
+        pub fn buy(&self) {
+            let caller = self.env().caller();
+            let value = self.env().transferred_balance();
+            assert!(value > 0, "msg.value cannot be zero");
+            let mut my_erc20: Erc20 = ink_env::call::FromAccountId::from_account_id(self.token_addr);
+            let hackt_balance = my_erc20.balance_of(self.env().account_id());
+            let transaction_value = value * (*self.exchange_rate);
+            assert!(hackt_balance >= transaction_value, "Insufficient balance on contract, transaction_value is {}, current balance is {}",
+            transaction_value, hackt_balance);
+            my_erc20.transfer(caller, transaction_value);
+
+        }
+
+        /// Simply returns the current value of our `bool`.
+        #[ink(message)]
+        pub fn exchange_rate(&self) -> u8 {
+            *self.exchange_rate
+        }
+    }
+}
